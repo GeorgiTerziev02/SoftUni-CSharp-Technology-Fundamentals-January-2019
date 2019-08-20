8501f9359ceb4ff8881fc96c7a4aa7a1dee68d63
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2019-08-20 11:12:24 +0300

    Add files via upload

---
+++ TeisterMask - Skeleton/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeisterMask.Data;
+using TeisterMask.Models;
+
+namespace TeisterMask.Controllers
+{
+    public class TaskController:Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db=new TeisterMaskDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+                return this.View(tasks);
+            }            
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+        [HttpPost]
+        public IActionResult Create(string title,string status)
+        {
+            if (string.IsNullOrEmpty(title)||string.IsNullOrEmpty(status))
+            {
+                return RedirectToAction("Index");
+            }
+            using (var db = new TeisterMaskDbContext())
+            {
+                Task task = new Task
+                {
+                    Title = title,
+                    Status=status
+                };
+                db.Tasks.Add(task);
+                db.SaveChanges();         
+            }
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToEdit = db.Tasks.Find(id);
+                return View(taskToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Update(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToDelete = db.Tasks.Find(id);
+                return View(taskToDelete);
+            }
+        }
+        [HttpPost]
+        public IActionResult Delete(Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Remove(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ TeisterMask - Skeleton/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeisterMask.Models;
+
+namespace TeisterMask.Data
+{
+    public class TeisterMaskDbContext:DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=TeisterMaskDb; Integrated Security=True;");
+        }
+
+    }
+}
